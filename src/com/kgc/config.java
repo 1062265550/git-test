b7b9905d6996f583c6c89c2dec4ad1f1b79d78ab
寒战 <18272122455@163.com>
2022-01-19 11:10:36 +0800

    这是我的第三次提交，sorry

--- src/com/kgc/config.java
+++ src/com/kgc/config.java
@@ -3,5 +3,6 @@ package com.kgc;
 public class config {
     public static void main(String[] args) {
         System.out.println("这是我想要提交的第二行代码");
+        System.out.println("这是我想要推送的代码，使用命令push");
     }
 }
