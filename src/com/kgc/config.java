d5360d9d826287306a9b332599fa8a51a698766f
1062265550 <97823825+1062265550@users.noreply.github.com>
2022-01-19 11:15:05 +0800

    Update config.java
    这个是我想要pull的代码

--- src/com/kgc/config.java
+++ src/com/kgc/config.java
@@ -1,8 +1,10 @@
-package com.kgc;
+ package com.kgc;
 
 public class config {
     public static void main(String[] args) {
         System.out.println("这是我想要提交的第二行代码");
         System.out.println("这是我想要推送的代码，使用命令push");
+        System.out.println("这是我想要pull的代码，使用的命令式pull");
+
     }
 }
